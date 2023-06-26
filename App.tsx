707fac48deec6ad83c3d90db406431583a896126
Aramis jaime <you@example.com>
2023-06-26 14:55:54 -0300

    i change routing

--- App.tsx
+++ App.tsx
@@ -1,17 +1,17 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import EmptyTask from './src/screens/emptyTaskScreen';
+import EmptyTask from './src/components/emptyTaskScreen/emptyTaskScreen';
 import TaskList from './src/screens/taskListScreen';
-import CreateNewTaskList from './src/screens/createNewTaskListScreen';
+import CreateNewTaskList from './src/screens/createNewTaskListScreen/createNewTaskListScreen';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="EmptyTask" component={EmptyTask} />
+        <Stack.Screen name="Home" component={TaskList} />
+        {/* <Stack.Screen name="EmptyTask" component={EmptyTask} /> */}
         <Stack.Screen name="NewTask" component={CreateNewTaskList} />
-        <Stack.Screen name="TaskList" component={TaskList} />
       </Stack.Navigator>
     </NavigationContainer>
   );
